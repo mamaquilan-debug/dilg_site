c6cf9b85832a0d79d4730a48e1b1c322b92daff8
Maica Maquilan <mamaquilan@dilg.gov.ph>
2026-07-25 15:31:39 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * db.php
- * Database connection for DILG Davao de Oro website
- */
-
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "region11_dilg_davaodeoro";
+$user = "region11_dilg_davaodeoro";      // the MySQL user you created
+$pass = "6b980add-2f82-4a7b-995d-031fdf867b51";     // that user's password
+$db   = "region11_dilg_davaodeoro";         // the database name you created
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
